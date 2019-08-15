70adfa151917b572f495caf7cb99e589349baf61
unknown <you@example.com>
2019-08-15 13:35:42 +0200

    first commit

---
+++ src/card.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import puce from './puce.jpg'
+import mastercard from './mastercard.jpg'
+function Card() {
+    return (<div className="card-name">
+        <center >
+            <h1> CREDIT CARD </h1>
+</center>
+        <div className='credit-card__logo' >
+            <img className='logo' src={puce} width="60" alt="logo" />
+            <div className='credit'>
+                
+                <h2 className="num">7253325b7895l245 </h2>
+                <div className='cardholder'>
+                    <div className='date'>
+                    <h2> 5422 </h2>
+                    <p> Month/Years</p>
+                    <h1>  11/50</h1>
+                    <img className='mastercard' src={mastercard} width="60" alt="card" />
+                    </div>
+                    <h2 className=" color">CARDHOLDER</h2>
+                </div>
+                
+            </div>
+
+
+        </div>
+
+    </div>)
+
+
+
+}
+export default Card
